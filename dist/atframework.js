3d3fa08f1eea70bfefd43bb9b3f7ed9f84c4646e
Andrey Tkachenko <atkachenko@griddynamics.com>
2015-04-15 17:04:48 +0300

    Remove Some useless code

--- dist/atframework.js
+++ dist/atframework.js
@@ -160,52 +160,7 @@ var ATF = {
             runnable[i]();
         }
     }
-};
-
-ATF.factory('$hash', [], function () {
-    return {
-        map: {},
-        objMap: {},
-        objKeys: {},
-        index: 0,
-        gen: function (prefix) {
-            return prefix + (this.index++);
-        },
-        hash: function (obj, recursive) {
-            var _h;
-            switch (typeof obj) {
-                case 'string':
-                    this.map[obj] = this.map[obj] || this.gen('s');
-                    return this.map[obj];
-                case 'number':
-                    this.map[obj] = this.map[obj] || this.gen('n');
-                    return this.map[obj];
-                case 'function':
-                    obj.$$hash = obj.$$hash || this.gen('f');
-                    return obj.$$hash;
-
-                case 'object':
-                    if (recursive) {
-                        _h = '';
-                        for (var key in obj) {
-                            if (obj.hasOwnProperty(key) && key.substr(0, 2) !== '$$') {
-                                this.objKeys[key] = this.objKeys[key] || this.gen('k');
-                                _h += '|' + this.objKeys[key] + ':' + this.hash(obj[key], true);
-                            }
-                        }
-
-                        this.objMap[_h] = this.objMap[_h] || this.gen('o');
-
-                        obj.$$hash = obj.$$hash === this.objMap[_h] ? obj.$$hash: this.objMap[_h];
-                    } else {
-                        obj.$$hash = obj.$$hash || this.gen('o');
-                    }
-
-                    return obj.$$hash;
-            }
-        }
-    };
-});;/*! AtFramework | Andrey Tkachenko | MIT License | github.com/andreytkachenko/atframework */
+};;/*! AtFramework | Andrey Tkachenko | MIT License | github.com/andreytkachenko/atframework */
 
 ATF.factory('utils', [], function () {
     return {
