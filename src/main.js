fffea6e25ea76ca442cd4026edd74acb23d45d7c
Andrey Tkachenko <atkachenko@griddynamics.com>
2015-04-17 16:13:42 +0300

    Dependency Injection Change Logick

--- src/main.js
+++ src/main.js
@@ -2,43 +2,113 @@
 
 var ATF = {
     initialized: false,
-    runindex: 0,
     dependencies: {},
-    invoking: [],
+    pending: [],
 
-    invoke: function (deps, func) {
-        if (this.initialized) {
+    _depsResolved: function (deps) {
+        for(var i = 0; i < deps.length; i++) {
+            if (!this.dependencies[deps[i]] ||
+                this.dependencies[deps[i]].resolved === false) {
+
+                return false;
+            }
+        }
+
+        return true;
+    },
+
+    invoke: function (deps, func, afterInitialize) {
+        var _resolved = this._depsResolved(deps);
+        if ((_resolved && !afterInitialize) || (_resolved && afterInitialize && this.initialized)) {
             func.apply(null, this.resolve(deps));
         } else {
-            this.invoking.push(arguments);
+            this.pending.push(arguments);
         }
     },
 
     resolve: function (deps) {
         var dependencies = this.dependencies;
-        return deps.map(function (i) {
+        return deps ? deps.map(function (i) {
             return dependencies[i].func();
-        });
+        }) : [];
+    },
+
+    invokePending: function () {
+        var len = this.pending.length;
+        while (len--)
+            this.invoke.apply(this, this.pending.shift());
+    },
+
+    resolveDependencies: function () {
+        var dependencies = this.dependencies;
+        var visited = {};
+
+        var resolve = function (name, visited) {
+            var item, failed = false;
+
+            if (!dependencies[name]) {
+                return false;
+            }
+
+            item = dependencies[name];
+
+            if (visited[name])
+                throw Error('Recursive dependency');
+
+            visited[name] = true;
+
+            if (item.resolved) {
+                return true;
+            }
+
+            if (item.deps) {
+                for (var i = 0; i < item.deps.length; i++) {
+                    if (resolve(item.deps[i], visited) === false) {
+                        failed = true;
+                    }
+                }
+
+                if (failed) {
+                    item.resolving = false;
+                    return false;
+                }
+            }
+
+            item.resolving = false;
+            item.resolved = true;
+        };
+
+        for (var i in this.dependencies) {
+            if (this.dependencies.hasOwnProperty(i)) {
+                if (this.dependencies[i].resolved || visited[i]) continue;
+
+                resolve(i, visited);
+            }
+        }
+
+        if (this.pending.length) {
+            this.invokePending();
+        }
     },
 
     register: function (name, deps, func, type) {
-        if (this.dependencies[name]) {
-            throw Error(name + ' is already registered!');
-        }
+        deps = deps || [];
 
         this.dependencies[name] = {
-            func: func(),
+            func: func,
             name: name,
             deps: deps,
             type: type,
             resolving: false,
-            resolved: false
+            resolved: !deps.length
         };
+
+        this.resolveDependencies();
     },
 
     controller: function (name, deps, func) {
         var self = this;
-        this.register(name, deps, function () {
+        this.register(name, deps, (function () {
             var called;
 
             return function () {
@@ -52,19 +122,17 @@ var ATF = {
                     scope: resolved[deps.indexOf('$scope')]
                 }
             }
-        }, 'controller');
+        })(), 'controller');
     },
     factory: function (name, deps, func) {
         var self = this;
         this.register(name, deps, function () {
-            return function () {
-                return func.apply(null, self.resolve(deps));
-            }
+            return func.apply(null, self.resolve(deps));
         }, 'factory');
     },
     service: function (name, deps, func) {
         var self = this;
-        this.register(name, deps, function () {
+        this.register(name, deps, (function () {
             var instance;
             return function () {
                 if (!instance) {
@@ -74,90 +142,45 @@ var ATF = {
 
                 return instance
             }
-        }, 'service');
+        })(), 'service');
     },
     view: function (name, deps, func) {
         var self = this;
         this.register(name, deps, function () {
-            return function () {
-                return func.apply(null, self.resolve(deps));
-            }
+            return func.apply(null, self.resolve(deps));
         }, 'view');
 
         return this;
     },
     config: function (name, value) {
         this.register(name, null, function () {
-            return function () {
-                return value;
-            }
+            return value;
         }, 'config');
 
         return this;
     },
+    value: function (name, value) {
+        this.register(name, null, function () {
+            return value;
+        }, 'value');
+
+        return this;
+    },
+
     run: function (deps, func) {
-        var self = this;
-        this.register('run' + this.runindex, deps, function () {
-            return function () {
-                return func.apply(null, self.resolve(deps));
-            }
-        }, 'run');
+        this.invoke(deps, func, true);
     },
 
     init: function () {
-        var runnable = [];
-        var dependencies = this.dependencies;
-
-        var resolve = function (name) {
-            var item;
-
-            if (!dependencies[name]) {
-                throw Error('Unknown dependency '+name);
-            }
-
-            item = dependencies[name];
-
-            if (item.resolving)
-                throw Error('Recursive dependency');
-
-            if (item.resolved) {
-                return;
-            }
-
-            item.resolving = true;
-
-            if (item.deps) {
-                for (var i = 0; i < item.deps.length; i++) {
-                    resolve(item.deps[i]);
-                }
-            }
-
-            item.resolving = false;
-            item.resolved = true;
-        };
-
         this.initialized = true;
 
-        if (this.invoking.length) {
-            for (var i=0; i < this.invoking.length; i++) {
-                this.invoke.apply(this, this.invoking[i]);
-            }
+        if (this.pending.length) {
+            this.invokePending();
         }
-
-        for (var i in this.dependencies) {
-            if (this.dependencies.hasOwnProperty(i)) {
-                if (this.dependencies[i].resolved) continue;
-                if (this.dependencies[i].resolving) throw Error('Recursive dependency');
-                resolve(i);
-
-                if (this.dependencies[i].type === 'run') {
-                    runnable.push(this.dependencies[i].func);
-                }
-            }
-        }
-
-        for (var i=0; i<runnable.length; i++) {
-            runnable[i]();
-        }
+    },
+    reset: function () {
+        this.dependencies = {};
+        this.pending = [];
+        this.initialized = false;
     }
 };
